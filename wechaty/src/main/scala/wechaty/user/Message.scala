4b80f071b28d85928468c60851204c8c34c66653
Jun Tsai <jcai@ganshane.com>
2020-06-04 14:14:24 +0800

    :sparkling: #8 初步调整支持java调用，因为scala的枚举在java调用产生问题，所以需要调整schemas的代码

--- wechaty/src/main/scala/wechaty/user/Message.scala
+++ wechaty/src/main/scala/wechaty/user/Message.scala
@@ -1,6 +1,6 @@
 package wechaty.user
 
-import wechaty.puppet.Puppet
+import wechaty.Wechaty.PuppetResolver
 import wechaty.puppet.schemas
 import wechaty.puppet.schemas.Puppet
 
@@ -9,10 +9,10 @@ import wechaty.puppet.schemas.Puppet
   * @author <a href="mailto:jcai@ganshane.com">Jun Tsai</a>
   * @since 2020-06-02
   */
-class Message(messageId:String)(implicit puppet:Puppet) {
-  lazy val payload: schemas.Message.MessagePayload = puppet.messagePayload(messageId)
+class Message(messageId:String)(implicit resolver: PuppetResolver) {
+  lazy val payload: schemas.Message.MessagePayload = resolver.puppet.messagePayload(messageId)
   def say(text:String): Unit = {
-    puppet.messageSendText(sayId(),text)
+    resolver.puppet.messageSendText(sayId(),text)
   }
   private def sayId(): String ={
     if(!Puppet.isBlank(payload.roomId)) payload.roomId
@@ -21,6 +21,6 @@ class Message(messageId:String)(implicit puppet:Puppet) {
   }
 
   override def toString: String = {
-    payload.text
+    if(payload!= null ) payload.text else ""
   }
 }
