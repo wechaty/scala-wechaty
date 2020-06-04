86d1f3964c7639d026055e736499e54de259dd22
Jun Tsai <jcai@ganshane.com>
2020-06-04 22:19:59 +0800

    #9 支持从系统变量中读取值

--- wechaty-hostie/src/main/scala/wechaty/hostie/Configuration.scala
+++ wechaty-hostie/src/main/scala/wechaty/hostie/Configuration.scala
@@ -6,6 +6,14 @@ package wechaty.hostie
   * @since 2020-06-02
   */
 object Configuration {
-  lazy val WECHATY_PUPPET_HOSTIE_TOKEN: Option[String] = sys.props.get("WECHATY_PUPPET_HOSTIE_TOKEN")
-  lazy val WECHATY_PUPPET_HOSTIE_ENDPOINT: Option[String] = sys.props.get("WECHATY_PUPPET_HOSTIE_ENDPOINT")
+  lazy val WECHATY_PUPPET_HOSTIE_TOKEN: Option[String] = {
+    var value=sys.props.get("WECHATY_PUPPET_HOSTIE_TOKEN")
+    if(value.isEmpty) value = sys.env.get("WECHATY_PUPPET_HOSTIE_TOKEN")
+    value
+  }
+  lazy val WECHATY_PUPPET_HOSTIE_ENDPOINT: Option[String] = {
+    var value = sys.props.get("WECHATY_PUPPET_HOSTIE_ENDPOINT")
+    if(value.isEmpty) value = sys.env.get("WECHATY_PUPPET_HOSTIE_ENDPOINT")
+    value
+  }
 }
